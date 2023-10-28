e9f2db76a8cc8a8722ceb9505d434077c681494b
Sabi <sabi456@gmail.com>
2023-10-27 22:12:13 -0600

    add more js

--- main.js
+++ main.js
@@ -6,12 +6,6 @@
 
 
 
-// const numberButton = document.querySelectorAll("numberButton");
-// numberButton.forEach(number => {
-//     number.addEventListener("click", function handleClick() {
-//         console.log("test");
-//     })
-// })
 
 
 // getElementsByClassName create a HTML Document which is similar to an array. The elements in a collection can be accessed by index (starts at 0).
@@ -22,7 +16,6 @@
 const numberButton = Array.from(document.getElementsByClassName("numberButton"));
 numberButton.forEach(number => {
     number.addEventListener('click', function setInputValue() {
-        console.log("clicked");
         let input = document.getElementById("operation");
         input.value += number.value;
     })
@@ -31,7 +24,6 @@ numberButton.forEach(number => {
 const operatorButton = Array.from(document.getElementsByClassName("operatorButton"));
 operatorButton.forEach(operator => {
     operator.addEventListener('click', function setInputValue() {
-        console.log("clicked something");
         let input = document.getElementById("operation");
         input.value += operator.value;
     })
@@ -40,11 +32,11 @@ operatorButton.forEach(operator => {
 
 const equalButton = document.getElementById("equalButton")
 equalButton.addEventListener("click", function() {
-    console.log("trying")
-    const compute = document.getElementById("operation").value;
-    let input = document.getElementById("result");
-    input.value = parseFloat(compute);
+    let result = document.getElementById("result");
+    result.value = (eval(document.getElementById("operation").value))
 })
+
+
 
 
 //gets the numbers in the display
@@ -58,27 +50,30 @@ equalButton.addEventListener("click", function() {
 
 const clearButton = document.getElementById("clearButton")
 clearButton.addEventListener("click", function(){
-    console.log("this works");
     document.getElementById("operation").value = '';
     document.getElementById("result").value = '';
 })
 
 const deleteButton = document.getElementById("deleteButton")
 deleteButton.addEventListener("click", function(){
+
+    // let input = document.getElementById("operation").value
+    // input = input.substring(0, input.length - 1);
+
+    // let a = document.getElementById("operation").value;
+    // a = a.substring(0, a.length -1);
+
+
+    let input = document.getElementById("operation").value;
+    input = input.slice(0, -1);
+
+
     console.log("delete please")
-    document.getElementById("operation").value -= document.getElementById("operation").value
+
+
+    // let str = document.getElementById("operation").value
+    // str = str.slice(0, -1);
+    // return str
+
+    // document.getElementById("operation").value -= document.getElementById("operation").value
 })
-
-// const operatorButton = document.getElementsByClassName("operator")
-// operatorButton.addEventListener("click", function(){
-
-
-
-
-// })inputs.forEach(input => {
-    // input.value = '';  (possible for delete?)
-
-
-
-
-// document.getElementById("operation").value += numberButton.innerText;
