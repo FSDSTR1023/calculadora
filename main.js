70ec0e65ba6223597658112066e9d4425accd393
Sabi <sabi456@gmail.com>
2023-10-28 14:32:19 -0600

    terminado la calculadora

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 
 
-
-
-
-
-
-
-
-
 // getElementsByClassName create a HTML Document which is similar to an array. The elements in a collection can be accessed by index (starts at 0).
 // otra manera de escribirlo: 
 // [...buttons].map(function(button) {
@@ -29,24 +21,12 @@ operatorButton.forEach(operator => {
     })
 })
 
-
 const equalButton = document.getElementById("equalButton")
 equalButton.addEventListener("click", function() {
     let result = document.getElementById("result");
     result.value = (eval(document.getElementById("operation").value))
+    document.getElementById("operation").value = '';
 })
-
-
-
-
-//gets the numbers in the display
-// const compute = document.getElementById("operation").value;
-
-
-
-
-    // document.getElementById("operation").value += numberButton.innerText
-    
 
 const clearButton = document.getElementById("clearButton")
 clearButton.addEventListener("click", function(){
@@ -56,24 +36,6 @@ clearButton.addEventListener("click", function(){
 
 const deleteButton = document.getElementById("deleteButton")
 deleteButton.addEventListener("click", function(){
-
-    // let input = document.getElementById("operation").value
-    // input = input.substring(0, input.length - 1);
-
-    // let a = document.getElementById("operation").value;
-    // a = a.substring(0, a.length -1);
-
-
-    let input = document.getElementById("operation").value;
-    input = input.slice(0, -1);
-
-
-    console.log("delete please")
-
-
-    // let str = document.getElementById("operation").value
-    // str = str.slice(0, -1);
-    // return str
-
-    // document.getElementById("operation").value -= document.getElementById("operation").value
+    const currentOperation = document.getElementById("operation");
+    currentOperation.value = currentOperation.value.slice(0,-1);
 })
