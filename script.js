62a5fe9f1126b9442cb7e57f7ccb98c635ae183c
alexander-lh <lasherasalexander@gmail.com>
2023-10-30 15:31:28 +0100

    new features added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let numberBtn = document.querySelectorAll('.numberBtn, .operationBtn')
+let numberBtn = document.querySelectorAll('.numberBtn')
+let operationBtn = document.querySelectorAll('.operationBtn')
 let operation = document.getElementById('operation')
 let result = document.getElementById('result')
 let equal = document.getElementById('equal')
 let clear = document.getElementById('clear')
+let backspace = document.getElementById('backspace');
 
 numberBtn.forEach(function(number) {
   number.addEventListener('click', function() {
@@ -11,12 +13,36 @@ numberBtn.forEach(function(number) {
   });
 });
 
+operationBtn.forEach(function(operationSign) {
+  operationSign.addEventListener('click', function() {
+    if (result.textContent === '' || operation.textContent !== '') {
+      operation.textContent += operationSign.textContent;
+      console.log('Hola');
+    } else {
+    console.log(operation.textContent);
+    operation.textContent +=  result.textContent + operationSign.textContent;
+    }
+  });
+});
+
+
 equal.addEventListener('click', function() {
-  result.textContent = eval(operation.textContent);
-  operation.textContent = '';
+  if (result.textContent === '' || operation.textContent !== '') {
+    result.textContent = eval(operation.textContent);
+    operation.textContent = '';
+  }
 })
 
 clear.addEventListener('click', function() {
   result.textContent = '';
   operation.textContent = '';
+})
+
+backspace.addEventListener('click', function() {
+    if(operation.textContent.length === 1 || operation.textContent === "ERROR!"){
+      operation.textContent = "";
+    }else{  
+      operation.textContent = operation.textContent.slice(0, -1);
+    }                
+    return;
 })
