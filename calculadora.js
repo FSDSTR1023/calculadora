bd20818f978049e355a1ee3e2503eeb62aa7bd7b
Enrique Garcia <enrique@amigosdelcara.com>
2023-11-01 12:53:56 +0100

    agrego funcionalidad de mp3 al pulsar botón

--- calculadora.js
+++ calculadora.js
@@ -24,62 +24,83 @@ function init(){
     var cero = document.getElementById('cero');
 }
 
+//audio de click
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+
 //Eventos de click
 uno.onclick = function(e){
     resultado.textContent = resultado.textContent  + "1";
+    play()
 }
 dos.onclick = function(e){
     resultado.textContent = resultado.textContent  + "2";
+    play()
 }
 tres.onclick = function(e){
     resultado.textContent = resultado.textContent  + "3";
+    play()
 }
 cuatro.onclick = function(e){
     resultado.textContent = resultado.textContent  + "4";
 }
 cinco.onclick = function(e){
     resultado.textContent = resultado.textContent  + "5";
+    play()
 }
 seis.onclick = function(e){
     resultado.textContent = resultado.textContent  + "6";
+    play()
 }
 siete.onclick = function(e){
     resultado.textContent = resultado.textContent  + "7";
+    play()
 }
 ocho.onclick = function(e){
     resultado.textContent = resultado.textContent  + "8";
+    play()
 }
 nueve.onclick = function(e){
     resultado.textContent = resultado.textContent  + "9";
+    play()
 }
 cero.onclick = function(e){
     resultado.textContent = resultado.textContent  + "0";
+    play()
 }
 reset.onclick = function(e){
     clear();
+    play()
 }
 suma.onclick = function(e){
     operandoa = resultado.textContent;
     operacion = "+";
     clear();
+    play()
 }
 resta.onclick = function(e){
     operandoa = resultado.textContent;
     operacion = "-";
     clear();
+    play()
 }
 multiplicacion.onclick = function(e){
     operandoa = resultado.textContent;
     operacion = "*";
     clear();
+    play()
 }
 division.onclick = function(e){
     operandoa = resultado.textContent;
     operacion = "/";
     clear();
+    play()
 }
 igual.onclick = function(e){
     operandob = resultado.textContent;
+    play()
     switch(operacion){
         case "+":
             add (parseFloat(operandoa),parseFloat(operandob));
